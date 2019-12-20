2de981076d1006b4e56e9b4241ae278789612ef9
ivin stephen <ivinstephen001@gmail.com>
2019-12-20 11:16:32 +0530

    added sub-category with admin inline fields

--- products/admin.py
+++ products/admin.py
@@ -7,6 +7,14 @@ from mptt.admin import DraggableMPTTAdmin
 from .models import Category
 
 
+class SubCategoryInline(admin.StackedInline):
+    model = Category
+    extra = 0
+    verbose_name = 'sub category'
+    verbose_name_plural = 'sub categories'
+    can_delete = False
+
+
 @admin.register(Category)
 class CategoryAdmin(DraggableMPTTAdmin):
     '''Admin View for Category'''
@@ -16,3 +24,4 @@ class CategoryAdmin(DraggableMPTTAdmin):
         # ...more fields if you feel like it...
     )
     list_display_links = ('indented_title',)
+    inlines = [SubCategoryInline]
