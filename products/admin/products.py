a88182c1aaa3ead705a9dadf5634629941d56dd3
ivin stephen <ivinstephen001@gmail.com>
2019-12-23 08:41:56 +0530

    admin seprated into different files

---
+++ products/admin/products.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+# 3rd party
+from mptt.admin import TreeRelatedFieldListFilter
+
+# local
+from ..models import Product, ProductImage, ProductReview
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    extra = 0
+    # verbose_name = 'sub category'
+    # verbose_name_plural = 'sub categories'
+    show_change_link = True
+
+
+class ProductReviewInline(admin.TabularInline):
+    model = ProductReview
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''Admin View for Product'''
+
+    list_display = ('name', 'price', 'descrption')
+    list_filter = (('category', TreeRelatedFieldListFilter),)
+    inlines = [
+        ProductImageInline,
+        ProductReviewInline,
+    ]
