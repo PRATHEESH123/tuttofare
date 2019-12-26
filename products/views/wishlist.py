7e93d98ebba5f8b744ac783f3527d062c5653a40
ivin stephen <ivinstephen001@gmail.com>
2019-12-26 18:07:32 +0530

    api for add and remove products from wishlist

--- products/views/wishlist.py
+++ products/views/wishlist.py
@@ -1,6 +1,7 @@
 # django rest framework
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 # local
@@ -15,3 +16,15 @@ class WishlistProductsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         user = self.request.user
         return super().get_queryset().filter(wishlist__user=user)
+
+    @action(detail=True)
+    def add(self, request, pk=None):
+        user = self.request.user
+        user.wishlist.products.add(pk)
+        return Response('products added')
+
+    @action(detail=True)
+    def remove(self, request, pk=None):
+        user = self.request.user
+        user.wishlist.products.remove(pk)
+        return Response('products removed')
