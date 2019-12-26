b05239c36d15845661cf382a685ce5584c991d0f
ivin stephen <ivinstephen001@gmail.com>
2019-12-26 14:54:12 +0530

    wishlist automatically created when new user created

--- products/models/wishlist.py
+++ products/models/wishlist.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+
+from django.conf import settings
+from django.dispatch import receiver
 
 
 class Wishlist(models.Model):
@@ -10,3 +14,9 @@ class Wishlist(models.Model):
     def __str__(self):
         """Unicode representation of Wishlist."""
         return f'[{self.user}]'
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_wishlist(sender, instance=None, created=False, **kwargs):
+    if created:
+        Wishlist.objects.create(user=instance)
