efff11e481e70439b6ea21155e5f74386dec8c95
ivin stephen <ivinstephen001@gmail.com>
2019-12-28 12:12:28 +0530

    items can be decrements from cart

--- orders/views/cart.py
+++ orders/views/cart.py
@@ -1,4 +1,10 @@
+# django
+from django.shortcuts import get_object_or_404
+
+# django rest framework
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ..models import Cart
 from ..serializers import CartItemSerializer, CartItemAddSerializer
@@ -12,3 +18,10 @@ class CartViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return CartItemAddSerializer
         return super().get_serializer_class()
+
+    @action(detail=True)
+    def decrement(self, request, pk):
+        cart = get_object_or_404(self.get_queryset(), pk=pk)
+        cart.quantity -= 1
+        cart.save()
+        return Response(self.get_serializer(cart).data)
