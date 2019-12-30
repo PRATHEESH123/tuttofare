6a449b3e57c5329291884832c5a24bd6837c1175
ivin stephen <ivinstephen001@gmail.com>
2019-12-30 13:07:37 +0530

    item admin panel

--- orders/admin/orders.py
+++ orders/admin/orders.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from ..models import Order
 
 
+class ItemInline(admin.TabularInline):
+    '''Tabular Inline View for Item'''
+
+    model = Order.items.through
+    max_num = 20
+    extra = 1
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     '''Admin View for Order'''
@@ -15,7 +23,7 @@ class OrderAdmin(admin.ModelAdmin):
         'payment_type',
         'shipping_address',
     )
-
+    inlines = [ItemInline]
     filter_horizontal = ('items',)
 
     def get_readonly_fields(self, request, obj=None):
