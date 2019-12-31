fff845e72e4ffe541dc6bb6e61ad9d797155478c
ivin stephen <ivinstephen001@gmail.com>
2020-01-01 05:00:03 +0530

    driver can set fullfilment status

--- orders/views/orders.py
+++ orders/views/orders.py
@@ -1,5 +1,10 @@
+# django
+from django.shortcuts import get_object_or_404
+
 # django rest framework
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # local
 from ..serializers import OrderSerializer, OrderCreateSerializer
@@ -16,3 +21,11 @@ class OrderViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return OrderCreateSerializer
         return super().get_serializer_class()
+
+    @action(detail=True)
+    def deliveried(self, request, pk):
+        order = get_object_or_404(Order, pk=pk)
+        order.fulfillment_status = Order.FULFILLED
+        order.payment_status = Order.PAID
+        order.save()
+        return Response(self.get_serializer(order).data)
