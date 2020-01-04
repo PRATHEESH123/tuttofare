9f8a3b0c94dc69c4b298c20ce34286a9b9938cdb
ivin stephen <ivinstephen001@gmail.com>
2020-01-04 18:22:52 +0530

    trying a basic example of translate

--- loun/middleware/translation_middleware.py
+++ loun/middleware/translation_middleware.py
@@ -1,16 +1,24 @@
 from django.http import HttpRequest, HttpResponse
+from googletrans import Translator, LANGUAGES
 
 
 class TranslationMiddleware:
 
     def __init__(self, get_response: HttpResponse):
         self.get_response = get_response
+        self.translator = Translator()
         # One-time configuration and initialization.
 
     def __call__(self, request: HttpRequest):
 
         response = self.get_response(request)
         response_text = response.content.decode("utf-8")
+        # print(response_text)
+
+        print(self.translate('hello').text)
 
         response.content = response_text.encode('utf-8')
         return response
+
+    def translate(self, text):
+        return self.translator.translate(text, dest='ar')
