f63c3e6999af6c93a7d0f2eaf2442e3c1ab73287
ivin stephen <ivinstephen001@gmail.com>
2020-01-05 00:43:30 +0530

    refactoring

--- loun/middleware/translation_middleware.py
+++ loun/middleware/translation_middleware.py
@@ -21,20 +21,19 @@ class TranslationMiddleware:
         response = self.get_response(request)
 
         if self.lang and self.lang != 'en':
-            response_text: str = response.content.decode("utf-8")
+            content = response.content.decode("utf-8")
+            content = self.pattern.sub(self.translation, content)
+            response.content = content.encode('utf-8')
 
-            response_text = self.pattern.sub(self.translate, response_text)
-
-            response.content = response_text.encode('utf-8')
         return response
 
     @lru_cache(maxsize=None)
-    def cache_translate(self, text, dest):
+    def cache_translation(self, text, dest):
         """ a seprate function is used to translate the match, in order
             to have differnet cache for each of the languages
         """
         return self.translator.translate(text, dest=dest, src='en')
 
-    def translate(self, match):
-        lang = self.cache_translate(match.group('value'), self.lang)
+    def translation(self, match):
+        lang = self.cache_translation(match.group('value'), self.lang)
         return ''.join((match.group('start'), lang.text, match.group('end')))
