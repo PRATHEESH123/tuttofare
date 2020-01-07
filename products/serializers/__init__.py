8afbaec2a80e85e8d3397f540d273a734a0f8ba7
ivin stephen <ivinstephen001@gmail.com>
2020-01-07 10:43:55 +0530

    brands API setup done

--- products/serializers/__init__.py
+++ products/serializers/__init__.py
@@ -2,10 +2,12 @@ from .categories import CategorySerializer
 from .collections import CollectionsSerializer
 from .products import ProductSerializer
 from .reviews import ProductReviewSerializer
+from .brands import BrandSerializer
 
 __all__ = (
     CategorySerializer,
     CollectionsSerializer,
     ProductSerializer,
     ProductReviewSerializer,
+    BrandSerializer,
 )
