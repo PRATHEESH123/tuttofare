218f45ea206a0289a7fc89dd4476fb8ac07e275f
PRATHEESH123 <pratheesh.b123@gmail.com>
2020-01-27 15:41:51 +0530

    brand translation done under product app

--- products/admin/brands.py
+++ products/admin/brands.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
+# local
 from ..models import Brand
 
+# tranlations
+from modeltranslation.admin import TranslationAdmin
 
 @admin.register(Brand)
-class BrandAdmin(admin.ModelAdmin):
+class BrandAdmin(TranslationAdmin):
     '''Admin View for Brand'''
 
     list_display = ('name',)
