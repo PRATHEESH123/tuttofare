5acc2e5e9440954974756252061b720a02ed589a
PRATHEESH123 <pratheesh.b123@gmail.com>
2020-01-27 15:42:35 +0530

    product app model registed under translation.py

--- products/translation.py
+++ products/translation.py
@@ -2,9 +2,25 @@
 from modeltranslation.translator import register, TranslationOptions
 
 # local
-from .models import Product
+from .models import Product , Category , Brand , Collection , Tag
 
 
 @register(Product)
 class ProductTranslationOptions(TranslationOptions):
-    fields = ('name', 'descrption')
+    fields = ('name', 'descrption',)
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(Brand)
+class BrandTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(Collection)
+class CollectionTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(Tag)
+class TagTranslationOptions(TranslationOptions):
+    fields = ('name',)
