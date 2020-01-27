4c1bf326ec32993b0312d3e0097c05cd0565964a
PRATHEESH123 <pratheesh.b123@gmail.com>
2020-01-27 15:35:38 +0530

    tag translation done under product model

--- products/admin/tags.py
+++ products/admin/tags.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 # local
 from ..models import Tag
 
+# tranlations
+from modeltranslation.admin import TranslationAdmin
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(TranslationAdmin):
     '''Admin View for Tag'''
 
     list_display = ('name',)
